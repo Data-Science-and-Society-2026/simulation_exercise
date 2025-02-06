ab89c30b75648034e6b5801533f84bb08bc814e8
DeTOB69 <tobiasjoling@hotmail.com>
2025-02-06 10:34:02 +0100

    Added some initial code to the tts_functions file, not working currently.

---
+++ tts_functions.py
@@ -0,0 +1,16 @@
+import whisper
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wavfile
+import ffmpeg
+
+def speech_to_text():
+    STTmodel = whisper.load_model('small.en')
+    print("Please speak now")
+    STTrecording = sd.rec(5*44100, samplerate=44100, channels=1, dtype=np.int16)
+    sd.wait(5)
+    wavfile.write("STTaudio.wav", 44100, STTrecording)
+    STTresult = STTmodel.transcribe("STTaudio.wav")
+    return STTresult["text"]
+
+speech_to_text()
