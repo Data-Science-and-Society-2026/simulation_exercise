0a8e61ad22ab45e2805fe6a1a88147c1619147fc
DeTOB69 <tobiasjoling@hotmail.com>
2025-02-11 14:40:36 +0100

    First version of both working TTS and STT functions

--- tts_functions.py
+++ tts_functions.py
@@ -2,6 +2,9 @@ import whisper
 import sounddevice as sd
 import numpy as np
 import scipy.io.wavfile as wavfile
+from TTS.api import TTS
+import torch
+import collections
 
 def speech_to_text():
     STTmodel = whisper.load_model('small.en')
@@ -12,4 +15,12 @@ def speech_to_text():
     STTresult = STTmodel.transcribe("STTaudio.wav")
     return STTresult["text"]
 
-speech_to_text()
+def text_to_speech(text):
+    
+    torch.serialization.add_safe_globals([dict, collections.defaultdict])
+
+    # Init TTS with the target model name
+    tts = TTS(model_name="tts_models/en/ek1/tacotron2", progress_bar=False).to("cpu")
+
+    # Run TTS
+    tts.tts_to_file(text=text, file_path="TTSaudio.wav")
